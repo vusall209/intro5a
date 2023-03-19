1c5a13313f3a1ccbe0c10f3a5d4febb9c125703a
vusall209 <vmammedov5@gmail.com>
2023-03-19 12:39:39 +0400

    Add project files.

---
+++ intro5a/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace intro5a
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello vdd World!");
+        }
+    }
+}
